5071deccb1c5b205b879ec86cc643c3de0e9020c
zhou <zhoujie0083@gmail.com>
2024-05-21 19:30:15 +0900

    出欠情報入力機能　作成中

--- src/pages/api/getAtdMbrInfo.ts
+++ src/pages/api/getAtdMbrInfo.ts
@@ -5,11 +5,11 @@ import { supabase } from "../../lib/supabase";
 //ATTEND_MEMBER_INFO(日程参加者の情報)の取得
 
 export const POST: APIRoute = async ({ request }) => {
-    const {schedule_id} = await request.json();
+    const { event_no } = await request.json();
     const { data, error } = await supabase
       .from("attend_member_info")
       .select("*")
-      .eq("schedule_id",schedule_id)
+      .eq("event_no", event_no)
       
     if (error) {
       return new Response(
